fd1cafe46d022110ddf938a4e29c0bdcdd64040b
Isaiah <computerblender@gmail.com>
2021-04-30 23:34:59 -0700

    Add tests for target box overlap detection

---
+++ src/logic/aabb.js
@@ -0,0 +1,4 @@
+// TODO: given two { x, y, width, height }, check if overlapping
+export default function getBoxCollision(boxA, boxB) {
+  return false;
+}
---
+++ src/tests/aabb.test.js
@@ -0,0 +1,35 @@
+import getCollisionBox from '../logic/aabb';
+
+describe('getCollisionBox()', () => {
+  test('should throw error if given invalid parameters', () => {
+    expect(() => getCollisionBox()).toThrowError();
+    expect(() => getCollisionBox(1)).toThrowError();
+    expect(() => getCollisionBox(1, 3)).toThrowError();
+    expect(() => getCollisionBox({x: 4, y: 4, width: 20, height: 20 })).toThrowError();
+  });
+  
+  test('should return true if box positions overlap', () => {
+    const boxA = {x: 1, y: 1, width: 2, height: 3};
+    const boxB = {x: 2, y: 3, width: 3, height: 3};
+    expect(getCollisionBox(boxA,boxB)).toEqual(true);
+  });
+
+  test('should return true if box positions are the same', () => {
+    const boxA = {x: 1, y: 1, width: 2, height: 3};
+    const boxB = {x: 1, y: 1, width: 2, height: 3};
+    expect(getCollisionBox(boxA,boxB)).toEqual(true);
+  })
+
+  test('should return false if box positions do not overlap', () => {
+    const boxA = {x: 1, y: 1, width: 2, height: 3};
+    const boxB = {x: 4, y: 3, width: 3, height: 3};
+    expect(getCollisionBox(boxA,boxB)).toEqual(false);
+  })
+
+  test('should return false if box positions are adjacent', () => {
+    const boxA = {x: 1, y: 1, width: 2, height: 3};
+    const boxB = {x: 3, y: 3, width: 3, height: 3};
+    expect(getCollisionBox(boxA,boxB)).toEqual(false);
+  })
+  
+})
