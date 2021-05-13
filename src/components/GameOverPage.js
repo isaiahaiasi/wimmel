7b52c5305ac92cc82a6dceae5793a72c1e80bf52
Isaiah <computerblender@gmail.com>
2021-05-12 23:57:28 -0700

    Begin logic for name input on highscore & add various game over messages

--- src/components/GameOverPage.js
+++ src/components/GameOverPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { addHighscore, useGetHighscores } from "../hooks/useFirestoreHooks";
 import displayTime from "../logic/display-time";
 import PageContext from "../PageContext";
@@ -7,34 +7,51 @@ import Scoreboard from "./Scoreboard";
 
 export default function GameOverPage({ time }) {
   const { handlePageChange, pages } = useContext(PageContext);
-  const score = {
-    score: time,
-    name: "test_name",
-  };
 
-  const [snapshot] = useGetHighscores();
+  const [showNameInput, setShowNameInput] = useState(false);
+  const [userName, setUserName] = useState("Charlie");
+  const [scoresSnapshot] = useGetHighscores();
 
-  console.log(snapshot);
-  const scores = snapshot?.docs?.map((score) => ({
+  const scores = scoresSnapshot?.docs?.map((score) => ({
     id: score.id,
     ...score.data(),
   }));
 
+  const didMakeHighscore = (score) =>
+    scores && (scores[scores.length - 1].score > score || scores.length < 5);
+
   useEffect(() => {
-    if (
-      scores &&
-      (scores[scores.length - 1].score > score.score || scores.length < 5)
-    ) {
-      console.log("score added!");
-      // TODO: get player's name, add their score to scoreboard
-      addHighscore(score);
+    if (didMakeHighscore(time)) {
+      setShowNameInput(true);
     }
   }, [scores]);
+
+  const handleAddScore = () => {
+    const score = {
+      score: time,
+      name: userName,
+    };
+    console.log("score added!");
+    // TODO: get player's name, add their score to scoreboard
+    addHighscore(score);
+  };
+
+  const getScoreMessage = (score) => {
+    if (didMakeHighscore(score)) {
+      if (score > scores[0]) {
+        return "Fantastic! You're number 1!!!";
+      } else {
+        return "High score! Great job!";
+      }
+    } else {
+      return "You found all the characters! Try again for a high score?";
+    }
+  };
 
   return (
     <S.ContainerCentered>
       <S.Window>
-        <h2>You won! Good job!</h2>
+        <h2>{getScoreMessage()}</h2>
         <div>Your time was {displayTime(time)}</div>
         <Scoreboard scores={scores} />
         <S.Button onClick={() => handlePageChange(pages.intro)}>
